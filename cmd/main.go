77b7c3533697e5bfdf1b4a4b00f2200d1d142abb
icoder-new <ehsonjonazimi05@gmail.com>
2023-10-17 18:56:02 +0500

    gracefully shutdown released

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fr33d0mz/moneyflowx"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+
+	srv := new(moneyflowx.Server)
+
+	go func() {
+		if err := srv.Run("", nil); err != nil {
+			return
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		return
+	}
+}
