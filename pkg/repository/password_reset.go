a674750afd4584fb7fa17191462143784ba5d8f4
icoder-new <ehsonjonazimi05@gmail.com>
2023-10-22 00:52:30 +0500

    created password_reset repo

---
+++ pkg/repository/password_reset.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"fr33d0mz/moneyflowx/models"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type PasswordResetRepository struct {
+	db *gorm.DB
+}
+
+func NewPasswordResetRepository(db *gorm.DB) *PasswordResetRepository {
+	return &PasswordResetRepository{
+		db: db,
+	}
+}
+
+func (p *PasswordResetRepository) FindByUserId(id string) (*models.PasswordReset, error) {
+	var passwordReset *models.PasswordReset
+
+	err := p.db.Where("user_id = ?", id).Find(&passwordReset).Error
+	if err != nil {
+		return passwordReset, err
+	}
+
+	return passwordReset, nil
+}
+
+func (p *PasswordResetRepository) FindByToken(token string) (*models.PasswordReset, error) {
+	var passwordReset *models.PasswordReset
+
+	err := p.db.Where("token = ?", token).Where("expired_at >= ?", time.Now()).Preload("User").Find(&passwordReset).Error
+	if err != nil {
+		return passwordReset, err
+	}
+
+	return passwordReset, nil
+}
+
+func (p *PasswordResetRepository) Save(passwordReset *models.PasswordReset) (*models.PasswordReset, error) {
+	err := p.db.Save(&passwordReset).Error
+	if err != nil {
+		return passwordReset, err
+	}
+
+	return passwordReset, nil
+}
+
+func (p *PasswordResetRepository) Delete(passwordReset *models.PasswordReset) (*models.PasswordReset, error) {
+	err := p.db.Delete(&passwordReset).Error
+	if err != nil {
+		return passwordReset, err
+	}
+
+	return passwordReset, nil
+}
