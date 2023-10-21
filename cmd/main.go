c0a2b5afb91fcfc09f636dd5476fc500702664c4
icoder-new <ehsonjonazimi05@gmail.com>
2023-10-21 23:46:14 +0500

    added parsed confs, logger, db conn

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"context"
 	"fr33d0mz/moneyflowx"
+	"fr33d0mz/moneyflowx/db"
+	"fr33d0mz/moneyflowx/logger"
+	"fr33d0mz/moneyflowx/utils"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 func main() {
+	utils.ReadSettings()
+	utils.PutAdditionalSettings()
+
+	logger.Init()
+	logger.Info.Println("[MAIN] logger working")
+
+	db.StartDbConnection()
+	_db := db.GetDBConn()
+	db.AutoMigrate(_db)
+	defer db.DisconnectDB(_db)
 
 	srv := new(moneyflowx.Server)
 
 	go func() {
 		if err := srv.Run("", nil); err != nil {
-			return
+			logger.Error.Fatalf("[MAIN] error occurred while running server: %v", err.Error())
 		}
 	}()
 
@@ -23,6 +36,6 @@ func main() {
 	<-quit
 
 	if err := srv.Shutdown(context.Background()); err != nil {
-		return
+		logger.Error.Fatalf("[MAIN] error occurred while shutdown server: %v", err.Error())
 	}
 }
