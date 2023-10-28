cc46f5fad10f8cdfaf557d818f0b76b9a7ace33c
icoder-new <ehsonjonazimi05@gmail.com>
2023-10-28 15:37:23 +0500

    GetStatusCode by custom errors

---
+++ utils/status_code.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"fr33d0mz/moneyflowx/utils/CustomError"
+	"net/http"
+)
+
+func GetStatusCode(err error) int {
+	var statusCode int = http.StatusInternalServerError
+
+	if _, ok := err.(*CustomError.NotValidEmailError); ok {
+		statusCode = http.StatusUnprocessableEntity
+	} else if _, ok := err.(*CustomError.UserAlreadyExistsError); ok {
+		statusCode = http.StatusConflict
+	} else if _, ok := err.(*CustomError.IncorrectCredentialsError); ok {
+		statusCode = http.StatusUnauthorized
+	} else if _, ok := err.(*CustomError.UserNotFoundError); ok {
+		statusCode = http.StatusBadRequest
+	} else if _, ok := err.(*CustomError.PasswordNotSameError); ok {
+		statusCode = http.StatusUnprocessableEntity
+	} else if _, ok := err.(*CustomError.ResetTokenNotFoundError); ok {
+		statusCode = http.StatusBadRequest
+	} else if _, ok := err.(*CustomError.SourceOfFundNotFoundError); ok {
+		statusCode = http.StatusBadRequest
+	} else if _, ok := err.(*CustomError.InsufficientBalanceError); ok {
+		statusCode = http.StatusBadRequest
+	} else if _, ok := err.(*CustomError.WalletNotFoundError); ok {
+		statusCode = http.StatusBadRequest
+	} else if _, ok := err.(*CustomError.WalletAlreadyExistsError); ok {
+		statusCode = http.StatusConflict
+	} else if _, ok := err.(*CustomError.TransferToSameWalletError); ok {
+		statusCode = http.StatusBadRequest
+	}
+
+	return statusCode
+}
